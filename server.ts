4c527deaf88c1c5b10caf90be373c4ab0184dce7
Zaikner <79411997+Zaikner@users.noreply.github.com>
2022-01-29 22:43:36 +0100

    heroku fixin

--- server.ts
+++ server.ts
@@ -4,18 +4,18 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 
-const Path = require('./editor/js/Path');
-//import {Socket} from './services/socket/Socket.js';
+
+import {Socket} from './services/socket/Socket.js';
 
 const app = express();
-//const server:Server = http.createServer(app);
+const server:Server = http.createServer(app);
 //const io = socketio(server);
 
-//const socketConnection = Socket.get();
-//socketConnection.setServerSocket(server);
+const socketConnection = Socket.get();
+socketConnection.setServerSocket(server);
 //socketConnection.setClientSocket('http://localhost:8001');
-//const io = socketConnection.getServerSocket()
-
+const io = socketConnection.getServerSocket()
+const Path = require('./editor/js/Path');
 
 
 app.use(express.static(__dirname));
@@ -23,18 +23,18 @@ const editor = require("./routes/editor.js")
 
 app.use('/',editor);
 const PORT = process.env.PORT || 8001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// io.on('connection', (socket:any) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//       console.log('user disconnected');
-//     });
-//     socket.on('skuska',() => {console.log("Client pripojeny, vysiela.")})
-//     socket.on('saveEditingTrack', (data:any) => {
-//         var newPath = new Path(data.points,data.type,data.length);
-//       console.log(newPath);
-//     });
-//   });
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+io.on('connection', (socket:any) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    socket.on('skuska',() => {console.log("Client pripojeny, vysiela.")})
+    socket.on('saveEditingTrack', (data:any) => {
+        var newPath = new Path(data.points,data.type,data.length);
+      console.log(newPath);
+    });
+  });
 
