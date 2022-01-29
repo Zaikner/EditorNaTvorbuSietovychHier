1989d56e5dca581b66c881c6c2de67913dd7590f
Zaikner <79411997+Zaikner@users.noreply.github.com>
2022-01-29 23:01:42 +0100

    Heroku fixin

--- server.ts
+++ server.ts
@@ -5,16 +5,16 @@ const http = require('http');
 const express = require('express');
 
 
-import {Socket} from './services/socket/Socket.js';
+//import {Socket} from './services/socket/Socket.js';
 
 const app = express();
 const server:Server = http.createServer(app);
 //const io = socketio(server);
 
-const socketConnection = Socket.get();
-socketConnection.setServerSocket(server);
+//const socketConnection = Socket.get();
+//socketConnection.setServerSocket(server);
 //socketConnection.setClientSocket('http://localhost:8001');
-const io = socketConnection.getServerSocket()
+//const io = socketConnection.getServerSocket()
 const Path = require('./editor/js/Path');
 
 
@@ -26,15 +26,15 @@ const PORT = process.env.PORT || 8001;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-io.on('connection', (socket:any) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-    socket.on('skuska',() => {console.log("Client pripojeny, vysiela.")})
-    socket.on('saveEditingTrack', (data:any) => {
-        var newPath = new Path(data.points,data.type,data.length);
-      console.log(newPath);
-    });
-  });
+// io.on('connection', (socket:any) => {
+//     console.log('a user connected');
+//     socket.on('disconnect', () => {
+//       console.log('user disconnected');
+//     });
+//     socket.on('skuska',() => {console.log("Client pripojeny, vysiela.")})
+//     socket.on('saveEditingTrack', (data:any) => {
+//         var newPath = new Path(data.points,data.type,data.length);
+//       console.log(newPath);
+//     });
+//   });
 
