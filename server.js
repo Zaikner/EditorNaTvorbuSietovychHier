a21592c5a622fd0b7eabb68ca717d1aa7ea73a6c
Zaikner <79411997+Zaikner@users.noreply.github.com>
2022-02-14 09:37:59 +0100

    socket server

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ var path = require('path');
 var http = require('http');
 var express = require('express');
 var a = require('./services/db/DbConnect.js');
-//import {Socket} from './services/socket/Socket.js';
+var Socket_js_1 = require("./services/socket/Socket.js");
 var app = express();
 var server = http.createServer(app);
 //const io = socketio(server);
-//const socketConnection = Socket.get();
-//socketConnection.setServerSocket(server);
-//socketConnection.setClientSocket('http://localhost:8001');
-//const io = socketConnection.getServerSocket()
+var socketConnection = Socket_js_1.Socket.get();
+socketConnection.setServerSocket(server);
+var io = socketConnection.getServerSocket();
 var Path = require('./editor/js/Path');
 app.use(express.static(__dirname));
 var editor = require("./routes/editor.js");
@@ -22,14 +21,9 @@ app.use('/editor', editor);
 app.use('/gameSpace', gameSpace);
 var PORT = process.env.PORT || 8001;
 server.listen(PORT, function () { return console.log("Server running on port ".concat(PORT)); });
-// io.on('connection', (socket:any) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//       console.log('user disconnected');
-//     });
-//     socket.on('skuska',() => {console.log("Client pripojeny, vysiela.")})
-//     socket.on('saveEditingTrack', (data:any) => {
-//         var newPath = new Path(data.points,data.type,data.length);
-//       console.log(newPath);
-//     });
-//   });
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
--- server.ts
+++ server.ts
@@ -5,16 +5,16 @@ const http = require('http');
 const express = require('express');
 const a = require('./services/db/DbConnect.js')
 
-//import {Socket} from './services/socket/Socket.js';
+import {Socket} from './services/socket/Socket.js';
 
 const app = express();
 const server:Server = http.createServer(app);
 //const io = socketio(server);
 
-//const socketConnection = Socket.get();
-//socketConnection.setServerSocket(server);
-//socketConnection.setClientSocket('http://localhost:8001');
-//const io = socketConnection.getServerSocket()
+
+const socketConnection = Socket.get();
+socketConnection.setServerSocket(server);
+const io = socketConnection.getServerSocket()
 const Path = require('./editor/js/Path');
 
 
@@ -29,15 +29,10 @@ const PORT = process.env.PORT || 8001;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// io.on('connection', (socket:any) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//       console.log('user disconnected');
-//     });
-//     socket.on('skuska',() => {console.log("Client pripojeny, vysiela.")})
-//     socket.on('saveEditingTrack', (data:any) => {
-//         var newPath = new Path(data.points,data.type,data.length);
-//       console.log(newPath);
-//     });
-//   });
+io.on('connection', (socket:any) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
 
