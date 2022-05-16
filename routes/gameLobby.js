01edead8addc6d31b69e2867503866787aaff9bf
Zaikner <79411997+Zaikner@users.noreply.github.com>
2022-05-16 16:23:18 +0200

    dd

--- routes/gameLobby.js
+++ routes/gameLobby.js
@@ -54,16 +54,28 @@ router
         //console.log(existingRoomsId)
 
         let filteredNames = []
+        let numOfPlayers = 6;
+        let size = '100000'
+        let questions = false;
         if(request.query.players!=undefined && request.query.tiles!= undefined && request.query.questions){
-    
+            numOfPlayers = request.query.players
+            if (request.query.questions == 'true'){
+                questions = true
+            }
+            else{
+                questions = false
+            }
+           
             let p = await GameFinder.getIntance().findByMaxPlayers(parseInt(request.query.players))
-            let size = 25;
-            if(request.query.tiles.substring(0,2) == 'St' || request.query.tiles.substring(0,2) == 'Mi'){
-               size = 50
-            }
-            else if (request.query.tiles.substring(0,2) == 'Lo' || request.query.tiles.substring(0,2) == 'Dl'){
-               size = 100000
-            }
+            size = parseInt(request.query.tiles)
+            //numOfTiles = 1
+            // if(request.query.tiles.substring(0,2) == 'St' || request.query.tiles.substring(0,2) == 'Mi'){
+            //    size = 50
+
+            // }
+            // else if (request.query.tiles.substring(0,2) == 'Lo' || request.query.tiles.substring(0,2) == 'Dl'){
+            //    size = 100000
+            // }
          
             let len = p.length
            
@@ -100,11 +112,11 @@ router
             filteredNames = a.slice()
         }
         if (request.query.full == undefined){
-            res.render('gameLobby.pug',{root:'./editor/views',gameNames:a,rooms:rooms,filteredNames:filteredNames,scores:sendScores,text:text,players:players,existingRoomsIds:existingRoomsIds});
+            res.render('gameLobby.pug',{root:'./editor/views',gameNames:a,rooms:rooms,filteredNames:filteredNames,scores:sendScores,text:text,players:players,existingRoomsIds:existingRoomsIds,numOfPlayers:numOfPlayers,questions:questions,numOfTiles:size.toString()});
         }
         else{
             console.log('aspon zachytil ze hra je plna')
-            res.render('gameLobby.pug',{root:'./editor/views',gameNames:a,rooms:rooms,filteredNames:filteredNames,scores:sendScores,text:text,players:players,full:true,existingRoomsIds:existingRoomsIds});
+            res.render('gameLobby.pug',{root:'./editor/views',gameNames:a,rooms:rooms,filteredNames:filteredNames,scores:sendScores,text:text,players:players,full:true,existingRoomsIds:existingRoomsIds,numOfPlayers:numOfPlayers,questions:questions,numOfTiles:size.toString()});
         }
         
         
@@ -178,91 +190,6 @@ router.route("/register")
 
 });
                 
-router.route("/players/:players/tiles/:tiles/questions/:questions")
-.get(async(request,res) =>
-{  
- console.log('parametre su:')
- console.log(request.params)
- let p = await GameFinder.getIntance().findByMaxPlayers(parseInt(request.params.players))
- let size = 25;
- if(request.params.tiles.substring(0,2) == 'St' || request.params.tiles.substring(0,2) == 'Mi'){
-    size = 50
- }
- else if (request.params.tiles.substring(0,2) == 'Lo' || request.params.tiles.substring(0,2) == 'Dl'){
-    size = 100000
- }
- let names = []
- let len = p.length
 
- for (let i = 0; i < len; i ++){
-    let game = p[i]
-    let gameInfo = await TileFinder.getIntance().findByGameId(game.getId())
-    console.log('dlzka je :'+ gameInfo.length)
-    if (gameInfo.length <= size){
-
-        let add = true;
-        if (request.params.questions == 'true'){
-            console.log('je true')
-            add = false
-            gameInfo.forEach((tile)=>{
-                if (tile.getQuestionId() >= 0 || tile.getRandomQuestion()){
-                    add = true
-                    console.log('nachadza sa otayzka')
-                }
-            })
-        }
-   
-        if (add){
-            names.push(game.getName())
-        }
-       
-        console.log('pridal')
-        console.log(names)
-     
-    }
-   
- }
-
-
- console.log('mena su:')
- console.log(names)
- let acc = AccountManager.getAccountByClientId(request.cookies.id)
-    let text;
-    if (request.cookies.language == 'SK'){
-        text =  (await TextsFinder.getIntance().findAll()).map((txt)=>txt.getSK())
-    }
-    else{
-        text =  (await TextsFinder.getIntance().findAll()).map((txt)=>txt.getEN())
-    }
-    let a = (await GameFinder.getIntance().findAllPublished()).map((game) => game.getName())
-    let rooms = Array.from(GameManager.getActiveRooms().values())
-    .map(room => [room.getId(),
-        "Room: "+room.getId()+ "  Game: "+ room.getGameName()+"   Players:  "+ room.getPlayers().length+'/'+room.getMaxPlayers(),
-        room.getGameName(),
-        room.getHasStarted()
-       ])
-    console.log('poslal rooms:')
-    console.log(rooms)
-    let scores =await AccountFinder.getIntance().findAllByOrderedScore()
-    let sendScores = []
-    for (let i = 1 ; i<= scores.length;i++){
-        sendScores.push([i,scores[i-1].getName(),scores[i-1].getScore()])
-    }
-    let players = GameManager.getActivePlayers(acc)
-    //console.log(rooms)
-    //console.log(sendScores)
-    let existingRoomsIds = Array.from(GameManager.getActiveRooms().keys())
-    console.log('existing rooms:')
-    //console.log(existingRoomsId)
-
-    if (request.query.full == undefined){
-        res.render('gameLobby.pug',{root:'./editor/views',gameNames:a,filteredNames:names,rooms:rooms,scores:sendScores,text:text,players:players,existingRoomsIds:existingRoomsIds});
-    }
-    else{
-        console.log('aspon zachytil ze hra je plna')
-        res.render('gameLobby.pug',{root:'./editor/views',gameNames:a,filteredNames:names,rooms:rooms,scores:sendScores,text:text,players:players,full:true,existingRoomsIds:existingRoomsIds});
-    }
- // res.render('register',{root:'./editor/views'
- })
 
 module.exports = router;
